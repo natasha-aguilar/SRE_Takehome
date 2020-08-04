e381e40a5efdd218e7b74265baf05275ad1a42d4
Natasha <natasha.aguilar@comcast.net>
2020-08-04 13:05:21 -0500

    updated

--- password_validator.py
+++ password_validator.py
@@ -3,19 +3,15 @@ import sys
 import fileinput
 import os
 
-#checks 8 char min, 
 def check_char_min_len(pwd):
     return len(pwd) >= 8
-
-#checks 64 char max     
+  
 def check_char_max_len(pwd):
     return len(pwd) <= 64
         
-#checks if ASCII char
 def check_is_ascii(pwd):
     return all(ord(char) < 128 for char in pwd)
        
-#checks for common pswd in weak_pwd list
 def check_is_common_pwd(pwd, weak_pswd_list):
     return pwd in weak_pswd_list
 
@@ -27,19 +23,17 @@ def check_valid_cmdline_args():
         print('Please provide a file as arg 2')
         exit()
 
-def main():
-    check_valid_cmdline_args()
-   
+def create_list_weak_pwds():
     weak_pswd_list = []
-
-    #create list of weak_passwords from file
     weak_pwds_file = open(sys.argv[1], 'r')
     pwds = weak_pwds_file.readlines()
+
     for weak_pswd in pwds:
         weak_pswd_list.append(weak_pswd.rstrip())
 
+    return weak_pswd_list
 
-    #checks each password in input file against criteria 
+def check_each_pwd_meets_criteria(weak_pswd_list):
     for input_pswd in sys.stdin:
         input_pswd = input_pswd.rstrip()
 
@@ -54,6 +48,13 @@ def main():
             continue
         elif check_is_common_pwd(input_pswd, weak_pswd_list):
             print(input_pswd + ' -> Error: Too Common')
+
+def main():
+    check_valid_cmdline_args()
+   
+    weak_pswd_list = create_list_weak_pwds()
+
+    check_each_pwd_meets_criteria(weak_pswd_list)
 
 
 if __name__ == "__main__":
